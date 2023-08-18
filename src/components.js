ef7f481211d98b7fff38a57e7bf3f7d1e1de4755
hyeblee <hyeb1224@naver.com>
2023-08-18 13:09:09 +0900

    08_18

---
+++ src/components.js
@@ -0,0 +1,5 @@
+function components(){
+    return <svg Xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="none"><ellipse cx="37.221" cy="45.458" fill="#FFA740" rx="37.221" ry="45.458" transform="matrix(-.12798 .99178 -.99514 -.09849 100 26.17)"/><path fill="#6CC26A" d="m50 0 6.908 17.815h22.356l-18.086 11.01 6.908 17.814L50 35.63 31.915 46.64l6.908-17.814-18.086-11.01h22.355L50 0Z"/></svg>
+}
+
+export default components;
